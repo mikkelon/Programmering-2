a914e40f051e990341af0058d915835d84e1e71a
Mikkel <mikkelovergaardnielsen@gmail.com>
2023-02-21 11:39:25 +0100

    opgave2 beregnBøde finished

---
+++ Lek08_Test/Lek08_Opgaver/src/opgave_2/Main.java
@@ -0,0 +1,27 @@
+package opgave_2;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Returnerer størrelsen af bøden beregnet i henhold til skemaet
+     * ovenfor
+     * krav: beregnetDato og faktiskDato indeholder lovlige datoer og
+     * beregnetDato < faktiskDato
+     * (beregnetDato er forventet afleveringsdato og
+     * faktiskDato er den dag bogen blev afleveret; voksen er
+     * sand, hvis det er en voksen og falsk ellers)
+     **/
+    public int beregnBøde(LocalDate beregnetDato, LocalDate faktiskDato, boolean voksen) {
+        int daysBetween = (int) (faktiskDato.toEpochDay() - beregnetDato.toEpochDay());
+
+        if (daysBetween == 0) return 0;
+        else if (daysBetween < 8) return voksen ? 20 : 10;
+        else if (daysBetween < 15) return voksen ? 60 : 30;
+        else return voksen ? 90 : 45;
+    }
+}
